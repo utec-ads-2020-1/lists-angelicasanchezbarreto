c8debf01356153b3e9f00e4a76e394fd35e8448c
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2020-04-17 15:25:21 -0500

    Queue

--- structures/queue.h
+++ structures/queue.h
@@ -17,12 +17,6 @@ public:
     {}
 };
 
-class CapacidadExcedida: public runtime_error {
-public:
-    CapacidadExcedida(const char *msg) : runtime_error( msg )
-    {}
-};
-
 
 template <typename T>
 class queue {
@@ -36,13 +30,23 @@ public:
     ~queue();
 
     void push(T);
-    int pop();
+    void pop();
     int front();
     int back();
 
     int size();
     bool empty();
+
+    void replace(T *nuevo);
 };
+
+template <typename T>
+void queue<T>::replace(T *nuevo) {
+    for(int i=0; i<size(); i++){
+        nuevo[i] = data[i];
+    }
+    data = nuevo;
+}
 
 template <typename T>
 queue<T>::queue(int size) {
@@ -54,32 +58,27 @@ queue<T>::queue(int size) {
 
 template <typename T>
 void queue<T>::push(T dato){
-    try{
-        if (pos_back >= capacity - 1)
-            throw CapacidadExcedida("Excede la capacidad");
-        else {
-            if(pos_front==-1)
-                pos_front=0;
+    if (pos_back >= capacity - 1){
+        capacity *= 2;
+        T* nuevo = new T[capacity];
+        replace(nuevo);
+        push(dato);
+    } else {
+        if(pos_front==-1)
+            pos_front=0;
 
-            pos_back++;
-            data[pos_back] = dato;
-        }
-    }
-    catch(const CapacidadExcedida &e) {
-        cerr << "ERROR: " << e.what() << endl;
+        pos_back++;
+        data[pos_back] = dato;
     }
 }
 
 template<typename T>
-int queue<T>::pop() {
+void queue<T>::pop() {
     try {
         if (empty())
             throw ArrayVacio("No existe data");
         else {
-            int first = data[pos_front];
-            pos_front++;
             pos_back--;
-            return first;
         }
     }
     catch (const ArrayVacio &e) {
@@ -93,8 +92,7 @@ int queue<T>::front() {
         if (empty())
             throw ArrayVacio("No existe data");
         else {
-            int first = data[pos_front];
-            return first;
+            return data[pos_front];
         }
     }
     catch (const ArrayVacio &e) {
@@ -108,8 +106,7 @@ int queue<T>::back() {
         if (empty())
             throw ArrayVacio("No existe data");
         else {
-            int last = data[pos_back];
-            return last;
+            return data[pos_back];
         }
     }
     catch (const ArrayVacio &e) {
@@ -119,7 +116,7 @@ int queue<T>::back() {
 
 template<typename T>
 int queue<T>::size() {
-    return capacity;
+    return pos_back+1;
 }
 
 template<typename T>
